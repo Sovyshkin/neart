9cb205cbdc88d5435ad4124ebac778a1119bce8c
Vadim <zankov.vadik@yandex.ru>
2025-01-13 19:47:13 +0500

    change color

--- app/utils/colors.ts
+++ app/utils/colors.ts
@@ -1,6 +1,7 @@
 import { pg_colors } from '../../themes/pg-tailwindcss/tokens.mjs'
 
-const primary = pg_colors.primary.DEFAULT
+const primary = '#aa6a2a'
+console.log("PRIMARY", primary)
 const secondary = pg_colors.secondary.DEFAULT
 
 const safelist = []
